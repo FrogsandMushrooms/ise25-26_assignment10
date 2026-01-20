4687cb9e84898d5978a762a90037e4d7d67c8cbe
FrogsandMushrooms <ig337@stud.uni-heidelberg.de>
2026-01-20 13:24:26 +0100

    edited conrete subject

--- de/seuhd/observer/ConcreteSubject.java
+++ de/seuhd/observer/ConcreteSubject.java
@@ -1,5 +1,20 @@
 package de.seuhd.observer;
 
-public class ConcreteSubject {
-    // TODO: implement concrete subject
+import java.util.ArrayList;
+
+public class ConcreteSubject extends Subject{
+
+    public ConcreteSubject(){
+        this.value = 0;
+        this.i = 0;
+
+    }
+
+    public void setValue(int newvalue){
+        this.value = newvalue;
+        System.out.print("Value of subject updated to ");
+        System.out.println(this.value);
+        this.notifyObservers();
+    }
+    
 }
