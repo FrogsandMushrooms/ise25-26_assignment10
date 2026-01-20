79562d849adffe972a276fc5b821f4f7645f283a
FrogsandMushrooms <ig337@stud.uni-heidelberg.de>
2026-01-20 13:23:19 +0100

    Edited ConcreteObserver

--- de/seuhd/observer/ConcreteObserver.java
+++ de/seuhd/observer/ConcreteObserver.java
@@ -1,18 +1,26 @@
 package de.seuhd.observer;
 
 public class ConcreteObserver implements Observer {
+    Subject connectedSubject;
+    String name;
     // TODO: Add fields and static variables.
 
-    public ConcreteObserver(Subject subject) {
+    public ConcreteObserver(Subject subject, String name) {
+        subject.attach(this);
+        connectedSubject = subject;
+        this.name = name;
         // TODO: Implement constructor.
     }
 
     @Override
     public void update(Subject subject) {
+        System.out.print(name);
+        System.out.print(" received update from subject: New value is " );
+        System.out.println(subject.value);
         // TODO: Implement update method.
     }
 
     public void detach() {
-        // TODO: Implement detach method.
+        connectedSubject.detach(this);
     }
 }
